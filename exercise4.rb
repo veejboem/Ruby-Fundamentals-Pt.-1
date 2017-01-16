ad8de998cb640e719384c7834b92e0331ce26f7c
Valerie Boem <vboem@gmail.com>
2017-01-16 16:58:58 -0500

    finished exercise4.rb

--- exercise4.rb
+++ exercise4.rb
@@ -1,6 +1,12 @@
-counter = 4
-
-while counter < 5
-  puts "counter currently at #{counter}"
-  counter += 1
+(1..100).each do |num|
+  
+if num % 3 ==0 && num % 5 == 0
+  puts "Bitmaker"
+elsif
+  num % 3 == 0
+  puts "Bit"
+  num % 5 == 0
+  puts "Maker"
+else puts num
+end
 end
